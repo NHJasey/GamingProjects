0f99ef85129ce9a712e2c37544b21553ca6073cf
Jasey <nhjasey@gmail.com>
2016-07-05 12:15:46 -0400

    Create main.cpp

---
+++ BullsAndCows/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+
+
+	// get guess from player and repeat it back
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	cout << "Your guess was: " << Guess << endl;
+
+
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
+	return 0;
+}

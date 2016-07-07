4a19a4053487d664fafedff9e089065f50ff6e28
Jasey <nhjasey@gmail.com>
2016-07-07 16:23:40 -0400

    Update main.cpp

--- BullsAndCows/src/main.cpp
+++ BullsAndCows/src/main.cpp
@@ -1,44 +1,111 @@
 #pragma once
-#include <string>
 
-using FString = std::string;
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+
+using FText = std::string;
 using int32 = int;
 
-struct FBullCowCount
+
+void PrintIntro();
+void PlayGame();
+FText GetValidGuess();
+bool AskToPlayAgain();
+void PrintGameSummary();
+
+FBullCowGame BCGame; 
+int main()
 {
-	int32 Bulls = 0;
-	int32 Cows = 0;
-};
+	bool bPlayAgain = false;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	}
+	while (bPlayAgain);
 
-enum class EGuessStatus
+	return 0;
+}
+
+void PrintIntro()
 {
-	Invalid_Status,
-	OK,
-	Not_Isogram,
-	Wrong_Length,
-	Not_Lowercase
-};
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << std::endl;
+	std::cout << "          }   {         ___ " << std::endl;
+	std::cout << "          (o o)        (o o) " << std::endl;
+	std::cout << "   /-------\\ /          \\ /-------\\ " << std::endl;
+	std::cout << "  / | BULL |O            O| COW  | \\ " << std::endl;
+	std::cout << " *  |-,--- |              |------|  * " << std::endl;
+	std::cout << "    ^      ^              ^      ^ " << std::endl;
+	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
+	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << std::endl;
+	return;
+}
 
-
-class FBullCowGame
+void PlayGame()
 {
-public:
-	FBullCowGame();
+	BCGame.Reset();
+	int32 MaxTries = BCGame.GetMaxTries();
+	
+	while (!BCGame.IsGameWon() && BCGame.GetCurrentTry() <= MaxTries) {
+		FText Guess = GetValidGuess();
 
-	int32 GetMaxTries() const;
-	int32 GetCurrentTry() const;
-	int32 GetHiddenWordLength() const;
-	bool IsGameWon() const;
-	EGuessStatus CheckGuessValidity(FString) const;
+		FBullCowCount BullCowCount = BCGame.SubmitValidGuess(Guess);
 
-	void Reset();
-	FBullCowCount SubmitValidGuess(FString);
+		std::cout << "Bulls = " << BullCowCount.Bulls;
+		std::cout << ". Cows = " << BullCowCount.Cows << "\n\n";
+	}
 
-private:
-	int32 MyCurrentTry;
-	FString MyHiddenWord;
-	bool bGameIsWon;
+	PrintGameSummary();
+	return;
+}
 
-	bool IsIsogram(FString) const;
-	bool IsLowercase(FString) const;
-};
+FText GetValidGuess()
+{
+	FText Guess = "";
+	EGuessStatus Status = EGuessStatus::Invalid_Status;
+	do {
+		int32 CurrentTry = BCGame.GetCurrentTry();
+		std::cout << "Try " << CurrentTry << " of " << BCGame.GetMaxTries();
+		std::cout << ". Enter your guess: ";
+		std::getline(std::cin, Guess);
+
+		Status = BCGame.CheckGuessValidity(Guess);
+		switch (Status) {
+		case EGuessStatus::Wrong_Length:
+			std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word.\n\n";
+			break;
+		case EGuessStatus::Not_Isogram:
+			std::cout << "Please enter a word witout repeating letters.\n\n";
+			break;
+		case EGuessStatus::Not_Lowercase:
+			std::cout << "Please enter all lowercase letters.\n\n";
+			break;
+		default:
+			break;
+		}
+	} while (Status != EGuessStatus::OK); 
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again with the same hidden word (y/n)? ";
+	FText Response = "";
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
+
+void PrintGameSummary()
+{
+	if (BCGame.IsGameWon())
+	{
+		std::cout << "WELL DONE - YOU WIN!\n";
+	}
+	else
+	{
+		std::cout << "Better luck next time!\n";
+	}
+}
